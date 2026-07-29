1847ef6d9b76a7cb780362f21d5240c6d07c2f6e
ritika-826 <vritika132@gmail.com>
2026-07-29 14:19:56 +0530

    Time: 1 ms (50.45%) | Memory: 14.2 MB (40.05%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.cpp
+++ 40-combination-sum-ii/combination-sum-ii.cpp
@@ -2,33 +2,18 @@ class Solution {
 public:
     vector<int> curr;
     vector<vector<int>> ans;
-
-    void solve(int idx,vector<int>& candidates,int target){
+    
+    void solve(int idx,vector<int>& candidates, int target){
      if(target==0){
         ans.push_back(curr);
         return;
-     }
-     if(target<0)return;
-     if(idx>=candidates.size())return;
-      for (int i = idx; i < candidates.size(); i++) {
-
-            // Skip duplicates at the same recursion level
-            if (i > idx && candidates[i] == candidates[i - 1])
-                continue;
-
-            // Since the array is sorted, no need to continue
-            if (candidates[i] > target)
-                break;
-
-            // Choose the current element
-            curr.push_back(candidates[i]);
-
-            // Move to the next index because each element
-            // can be used only once
-            solve(i + 1, candidates, target - candidates[i]);
-
-            // Backtrack
-            curr.pop_back();
+    }
+    if(target<0 || idx >=candidates.size())return;
+    for(int i=idx;i<candidates.size();i++){
+        if(i>idx && candidates[i]==candidates[i-1])continue;
+        curr.push_back(candidates[i]);
+        solve(i+1,candidates,target-candidates[i]);
+        curr.pop_back();
     }
 }
     vector<vector<int>> combinationSum2(vector<int>& candidates, int target) {
@@ -36,4 +21,5 @@ public:
         solve(0,candidates,target);
         return ans;
     }
+    
 };
