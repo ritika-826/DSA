99065ad59f24ccf659194347d7f5489ca9626082
ritika-826 <vritika132@gmail.com>
2026-08-22 14:45:11 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (43.64%) - LeetSync

--- 59-spiral-matrix-ii/spiral-matrix-ii.cpp
+++ 59-spiral-matrix-ii/spiral-matrix-ii.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> generateMatrix(int n) {
-        vector<vector<int>> arr(n,vector<int> (n,0));
+        vector<vector<int>> arr(n,vector<int>(n,0));
         int r=0;
         int c=0;
         int dir=0;
+        // R,D,L,U
         int dr[]={0,1,0,-1};
         int dc[]={1,0,-1,0};
         for(int i=1;i<=n*n;i++){
             arr[r][c]=i;
             int nr=r+dr[dir];
             int nc=c+dc[dir];
-            if(nr < 0 || nr >=n || nc <0 || nc >=n || arr[nr][nc]!=0){
+            if(nr<0 || nr >=n || nc <0 || nc>=n || arr[nr][nc]!=0){
                 dir=(dir+1)%4;
-                
             }
             r=r+dr[dir];
             c=c+dc[dir];
 
-        }
 
+           
+        }
         return arr;
-        
     }
 };
