4b298123f6e81061827f7cc6d04d0e62ea7e673f
ritika-826 <vritika132@gmail.com>
2026-09-03 23:07:10 +0530

    Time: 0 ms (100.00%) | Memory: 16.7 MB (86.15%) - LeetSync

--- 1036-rotting-oranges/rotting-oranges.cpp
+++ 1036-rotting-oranges/rotting-oranges.cpp
@@ -1,53 +1,46 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int row=grid.size();
-        int col=grid[0].size();
-        queue<pair<int,int>> q;
-        int fresh=0;
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(grid[i][j]==1)fresh++;
+        int m=grid.size();queue<pair<int,int>>q;
+        int n=grid[0].size();
+        int time=0;int fresh=0;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(grid[i][j]==2){
                     q.push({i,j});
                 }
-            }
-        }
-        int minute=0;
-        int dr[]={-1,1,0,0};
-        int dc[]={0,0,-1,1};
-        while(!q.empty()){
-            int size=q.size();
-            bool changed=false;
-            for(int i=0;i<size;i++){
-                auto node=q.front();
-                q.pop();
-                int r=node.first;
-                int c=node.second;
-                for(int k=0;k<4;k++){
-                    
-                    int nr=r+dr[k];
-                    int nc=c+dc[k];
-                     if(nr>=0 && nr<row &&
-                        nc>=0 && nc<col &&
-                        grid[nr][nc]==1
-                        ){
-                            grid[nr][nc]=2;
-                            fresh--;
-                            q.push({nr,nc});
-                            changed=true;
-
-                        }
-
+                if(grid[i][j]==1){
+                    fresh++;
                 }
-
-
             }
-            if (changed)
-            minute++;
-        }
-        if(fresh>0) return -1;
-        return minute;
+        }       
+                
+                 int dr[] = {-1, 1, 0, 0};
+                 int dc[] = {0, 0, -1, 1};
+                while(!q.empty()&& fresh>0){
+                    int sz=q.size();
+                    // ek level ek minute
+                    while(sz--){
+                        auto[r,c]=q.front();
+                        q.pop();
+                        for(int k=0;k<4;k++){
+                        int nr=r+dr[k];
+                        int nc=c+dc[k];
+                        if (nr >= 0 && nr < m &&
+                        nc >= 0 && nc < n &&
+                        grid[nr][nc] == 1) {
 
+                        grid[nr][nc] = 2;
+                        fresh--;
+
+                        q.push({nr, nc});
+                        }
+                    }
+                }
+                time++;
+            }
+            if(fresh>0)return -1;    
+        
+        return time;
     }
 };
