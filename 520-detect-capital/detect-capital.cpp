62d1911db64d6eef4cd1086076fe40d56f1fe292
ritika-826 <vritika132@gmail.com>
2026-09-10 20:42:24 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (51.01%) - LeetSync

---
+++ 520-detect-capital/detect-capital.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int count=0;
+        for(char ch:word){
+            if(isupper(ch))count++;
+        }
+        if(count==word.length())return true;
+        if(count==0)return true;
+        if(count==1 && isupper(word[0]))return true;
+        return false;
+    }
+};
